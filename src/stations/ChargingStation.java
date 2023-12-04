b2bea2026bfbb2ed43b073ba5f36a4f7c822e0c8
allerter <a.hazhir@gmail.com>
2023-12-04 18:25:51 +0100

    Add missing level of gas and electricity storage parameters

--- src/stations/ChargingStation.java
+++ src/stations/ChargingStation.java
@@ -43,8 +43,10 @@ public class ChargingStation {
 			GPSValues gpsValues,
 			int numGasSlots,
 			int numElectricSlots,
-			float gasOutputPerSecondoutputPerSecond,
-			float electricityOutputPerSecond)
+			float gasOutputPerSecond,
+			float electricityOutputPerSecond,
+			float LevelOfElectricityStorage,
+			float LevelOfGasStorage)
 			throws InvalidGPSLatitudeException, InvalidGPSLongitudeException, InvalidGPSValueException {
 		this.logger = Logger.getLogger(this.toString());
 		this.chargingStationID = chargingStationID;
@@ -99,6 +101,19 @@ public class ChargingStation {
 		this.gasOutputPerSecond = gasOutputPerSecondoutputPerSecond;
 		this.electricityOutputPerSecond = electricityOutputPerSecond;
 
+		if (LevelOfElectricityStorage < 0 || LevelOfGasStorage < 0) {
+			throw new IllegalArgumentException("Charging station storage can't be fewer than 0.");
+		} else if (LevelOfElectricityStorage == 0 && LevelOfGasStorage == 0){
+			throw new IllegalArgumentException("Station can't have 0 storage of any kind");
+		}
+		if (numGasSlots == 0 && LevelOfGasStorage > 0) {
+			throw new IllegalArgumentException("Station can't have 0 gas slots and still have gas output potential.");
+		} else if (numElectricSlots == 0 && LevelOfElectricityStorage > 0) {
+			throw new IllegalArgumentException(
+					"Station can't have 0 electricity slots and still have electricity output potential.");
+		}
+		this.LevelOfElectricityStorage = LevelOfElectricityStorage;
+		this.LevelOfGasStorage = LevelOfGasStorage;
 		this.logger.fine("Initiated " + this.toString());
 	}
 
