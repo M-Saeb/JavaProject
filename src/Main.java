80c9677fabc3423ce743fa9e87928be7bb6d107e
Luis Fernando <luis_rogu@outlook.com>
2023-12-14 10:04:04 +0100

    test for full concurrency and scalability

--- src/Main.java
+++ src/Main.java
@@ -4,59 +4,14 @@ import java.util.concurrent.Semaphore;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
-class ChargingSlots {
-    private final Semaphore semaphore;
-
-    public ChargingSlots(int numberOfSlots) {
-        this.semaphore = new Semaphore(numberOfSlots, true);
-    }
-
-    public boolean tryAcquireSlot(Car car) {
-        try {
-            System.out.println(car.getType() + " car is trying to acquire a slot.");
-            return semaphore.tryAcquire();
-        } catch (Exception e) {
-            return false;
-        }
-    }
-
-    public void releaseSlot(Car car) {
-        System.out.println(car.getType() + " car released the slot.");
-        semaphore.release();
-    }
-}
-
-class Car {
-    private final String type;
-    private long entryTime;
-
-    public Car(String type) {
-        this.type = type;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public long getEntryTime() {
-        return entryTime;
-    }
-
-    public void setEntryTime(long entryTime) {
-        this.entryTime = entryTime;
-    }
-
-    public void charge(ChargingStation chargingStation) {
-        chargingStation.chargeCar(this);
-    }
-}
-
 class ChargingStation {
     private final ChargingSlots slots;
     private final List<Car> waitingQueue;
     private final Lock stationLock;
+    private final int id;
 
-    public ChargingStation(int numberOfSlots) {
+    public ChargingStation(int id, int numberOfSlots) {
+        this.id = id;
         this.slots = new ChargingSlots(numberOfSlots);
         this.waitingQueue = new ArrayList<>();
         this.stationLock = new ReentrantLock();
@@ -67,12 +22,12 @@ class ChargingStation {
             stationLock.lock();
             if (slots.tryAcquireSlot(car)) {
                 // Charging logic here
-                System.out.println(car.getType() + " car is charging.");
+                System.out.println(car.getType() + " car " + car.getId() + " is charging at Station " + id + ".");
                 slots.releaseSlot(car);
             } else {
                 // If slots are full, enqueue the car with entry time
                 car.setEntryTime(System.currentTimeMillis());
-                System.out.println(car.getType() + " car is waiting in the queue.");
+                System.out.println(car.getType() + " car " + car.getId() + " is waiting in the queue at Station " + id + ".");
                 waitingQueue.add(car);
             }
         } finally {
@@ -91,7 +46,8 @@ class ChargingStation {
                 if (waitingTime <= 15 * 1000) { // 15 seconds in milliseconds
                     chargeCar(car); // Try to charge the car again
                 } else {
-                    System.out.println(car.getType() + " car left the queue after waiting too long.");
+                    System.out.println(car.getType() + " car " + car.getId() + " left the queue at Station " + id +
+                            " after waiting too long.");
                 }
             }
         } finally {
@@ -100,31 +56,90 @@ class ChargingStation {
     }
 }
 
-public class main {
+class ChargingSlots {
+    private final Semaphore semaphore;
+
+    public ChargingSlots(int numberOfSlots) {
+        this.semaphore = new Semaphore(numberOfSlots, true);
+    }
+
+    public boolean tryAcquireSlot(Car car) {
+        try {
+            System.out.println(car.getType() + " car " + car.getId() + " is trying to acquire a slot.");
+            return semaphore.tryAcquire();
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public void releaseSlot(Car car) {
+        System.out.println(car.getType() + " car " + car.getId() + " released the slot.");
+        semaphore.release();
+    }
+}
+
+class Car {
+    private final String type;
+    private final int id;
+    private long entryTime;
+
+    public Car(int id, String type) {
+        this.id = id;
+        this.type = type;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public long getEntryTime() {
+        return entryTime;
+    }
+
+    public void setEntryTime(long entryTime) {
+        this.entryTime = entryTime;
+    }
+
+    public void charge(ChargingStation chargingStation) {
+        chargingStation.chargeCar(this);
+    }
+}
+
+public class Main {
     public static void main(String[] args) {
-        int numberOfSlots = 5;
-        ChargingStation chargingStation = new ChargingStation(numberOfSlots);
+        int numberOfSlotsPerStation = 5;
+        int numberOfStations = 5;
+        int numberOfCars = 20;
 
-        Thread stationThread = new Thread(() -> {
-            // Your logic for the charging station thread
-            // For example, you might periodically release queued cars
-            while (true) {
-                chargingStation.releaseQueuedCars();
-                try {
-                    Thread.sleep(5000); // Simulating a delay
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+        List<ChargingStation> chargingStations = new ArrayList<>();
+
+        for (int i = 0; i < numberOfStations; i++) {
+            ChargingStation chargingStation = new ChargingStation(i + 1, numberOfSlotsPerStation);
+            chargingStations.add(chargingStation);
+
+            Thread stationThread = new Thread(() -> {
+                while (true) {
+                    chargingStation.releaseQueuedCars();
+                    try {
+                        Thread.sleep(5000); // Simulating a delay
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
                 }
-            }
-        });
-        stationThread.start();
+            });
+            stationThread.start();
+        }
 
-        int numberOfCars = 10;
         for (int i = 0; i < numberOfCars; i++) {
             String carType = i % 2 == 0 ? "Electric" : "Gas";
-            Car car = new Car(carType);
+            Car car = new Car(i + 1, carType);
 
-            // Logic for charging cars is now in the Car class
+            ChargingStation chargingStation = chargingStations.get(i % numberOfStations);
+
             Thread chargingThread = new Thread(() -> car.charge(chargingStation));
             chargingThread.start();
         }
